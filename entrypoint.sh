1f7d62f29ff69212e6e17764ff8a4d13d9f548f9
MMphepo <164493503+MMphepo@users.noreply.github.com>
2026-09-16 01:59:15 +0200

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,15 @@
+```sh
 #!/bin/sh
 
+set -e
+
+echo "Running Blnk database migrations..."
+
+blnk migrate up
+
+echo "Blnk migrations completed successfully."
+
+echo "Starting Blnk..."
+
 exec blnk start
+```
